8955cfa59e1e2023769d10f0218458d804e02041
mulv3959 <mulv3959@stthomas.edu>
2014-08-14 15:54:34 -0500

    added user authentication and password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :users, only: [:none] do
+    resources :password_resets, only: [:edit, :update]
+  end
+  resources :password_resets, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
